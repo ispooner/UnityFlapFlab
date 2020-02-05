f9a2dd5dca28af7c088bc87c94903b29e7118a5f
ISpooner <cbk2011ian@gmail.com>
2020-02-05 08:43:59 -0500

    Initial Commit
    All the things

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Background : MonoBehaviour
+{
+    public Vector3 startPos;
+
+    public float xScale = 1.14f;
+    public Sprite background;
+    public float repeatLength = 21.66f;
+    public float scrollspeed;
+    private float newPos = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+        rend.sprite = background;
+        transform.localScale = new Vector3(xScale, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * scrollspeed, repeatLength);
+        transform.position = startPos + Vector3.right * newPos;
+    }
+}
---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public Sprite[] layers;
+    public GameObject backgroundObject;
+    public float speedModifier = default;
+    public float baseSpeed = default;
+    public int depth = default;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < layers.Length; i++)
+        {
+            GameObject temp = Instantiate<GameObject>(backgroundObject, new Vector3(-21.66f, 0, depth + i), Quaternion.identity, transform);
+            Background back = temp.GetComponent<Background>();
+            back.background = layers[i];
+            back.scrollspeed = baseSpeed * (Mathf.Pow( speedModifier, i));
+            
+            temp = Instantiate<GameObject>(backgroundObject, new Vector3(0f, 0, depth + i), Quaternion.identity, transform);
+            back = temp.GetComponent<Background>();
+            back.background = layers[i];
+            back.scrollspeed = baseSpeed * (Mathf.Pow(speedModifier, i));
+        }
+    }
+}
